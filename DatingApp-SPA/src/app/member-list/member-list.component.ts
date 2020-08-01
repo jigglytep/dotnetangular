2ffd8cb254a41246d3b1d86c405ae3f671004303
--list <ltsenovoy@gmail.com>
2020-07-31 21:10:24 -0400

    profile final, deployed to elastic beanstalk

--- DatingApp-SPA/src/app/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/member-list/member-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  pdfSrc = "assets/pdf/LevTsenovoy.PDF";
+  pdfSrc = "assets/pdf/LevTsenovoy.pdf";
   constructor() { }
 
   ngOnInit() {
