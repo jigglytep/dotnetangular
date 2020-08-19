88b47b0bdfb58a880a910f82ed8f83aaefbf4354
--list <ltsenovoy@gmail.com>
2020-08-18 23:28:02 -0400

    backing up

--- API/Dtos/PhotosForDetailedDto.cs
+++ API/Dtos/PhotosForDetailedDto.cs
@@ -1,13 +1,13 @@
 using System;
 
-namespace DatingApp.API.Data
+namespace DatingApp.API.Dtos
 {
     public class PhotosForDetailedDto
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
-        public DateTime DateAdded { get; set; }
+        public DateTime DateAeed { get; set; }
         public bool IsMain { get; set; }
     }
 }
